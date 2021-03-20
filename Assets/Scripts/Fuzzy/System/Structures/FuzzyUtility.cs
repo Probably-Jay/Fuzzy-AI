d990e597087aeeb9055cadfd3fa2d03a22652db6
Jay Bretherton <jake.bretherton05@gmail.com>
2021-03-20 21:43:09 +0000

    helper functions

--- Assets/Scripts/Fuzzy/System/Structures/FuzzyUtility.cs
+++ Assets/Scripts/Fuzzy/System/Structures/FuzzyUtility.cs
@@ -1,4 +1,6 @@
-﻿namespace FuzzyLogic
+﻿using UnityEngine;
+
+namespace FuzzyLogic
 {
 
     public static class FuzzyUtility
@@ -11,6 +13,88 @@
             , MP
             , LP
         }
+
+        /// <summary>
+        /// Clamped normalises <paramref name="value"/> to a <c>-1</c> to <c>1</c> scale based on <paramref name="valueMin"/> and <paramref name="valueMax"/>
+        /// </summary>
+        /// <param name="valueMin">The minumum value this <paramref name="value"/> can be expeceted to have, will be mapped to <c>-1</c></param>
+        /// <param name="valueMax">The maximum value this <paramref name="value"/> can be expeceted to have, will be mapped to <c>1</c></param>
+        /// <param name="value">The value to be normalised</param>
+        /// <returns>Normalised represenation of <paramref name="value"/> within the range <c>[-1,1]</c></returns>
+        public static float NormaliseValue(float valueMin, float valueMax, float value)
+        {
+            float zeroOne = Mathf.Clamp01(Mathf.InverseLerp(valueMin, valueMax, value));
+            zeroOne *= 2f;
+            zeroOne -= 1;
+            return zeroOne;
+        }
+
+        /// <summary>
+        /// Clamped normalises <paramref name="value"/> to a <c>-1</c> to <c>1</c> scale based on the uneven scale between <paramref name="valueMin"/>, <paramref name="valueNeutral"/>, and <paramref name="valueMax"/>
+        /// </summary>
+        /// <param name="valueMin">The minumum value this <paramref name="value"/> can be expeceted to have, will be mapped to <c>-1</c></param>
+        /// <param name="valueNeutral">The value representing a neutral state of <paramref name="value"/>, will be mapped to <c>0</c></param>
+        /// <param name="valueMax">The maximum value this <paramref name="value"/> can be expeceted to have, will be mapped to <c>1</c></param>
+        /// <param name="value">The value to be normalised</param>
+        /// <returns>Normalised represenation of <paramref name="value"/> within the range <c>[-1,1]</c></returns>
+        public static float NormaliseValueUneven(float valueMin, float valueNeutral, float valueMax, float value)
+        {
+            if (value < valueNeutral)
+            {
+                float negZero = Mathf.Clamp01(Mathf.InverseLerp(valueMin, valueNeutral, value));
+                negZero -= 1;
+                return negZero;
+            }
+            else if (value > valueNeutral)
+            {
+                float zeroOne = Mathf.Clamp01(Mathf.InverseLerp(valueNeutral, valueMax, value));
+                return zeroOne;
+            }
+            else return 0; // mapp neutral to 0
+           
+        }
+
+        /// <summary>
+        /// Returns a value that was normalised to be between <c>[-1,1]</c> to again be between the clamped range <c>[</c><paramref name="valueMin"/><c>,</c><paramref name="valueMax"/><c>]</c>
+        /// </summary>
+        /// <param name="valueMin">The minumum value this function will return, will be mapped from <c>-1</c></param>
+        /// <param name="valueMax">The maximum value this function will return, will be mapped from <c>1</c></param>
+        /// <param name="nValue">The normalised value to be projected, valid over range <c>[-1,1]</c></param>
+        /// <returns>Un-Normalised value</returns>
+        public static float UnNormaliseValue(float valueMin, float valueMax, float nValue)
+        {
+            float negPosOne = Mathf.Clamp(nValue,-1,1);
+            negPosOne += 1;
+            negPosOne /= 2;
+            return Mathf.Lerp(valueMin, valueMax, negPosOne);
+        }
+
+        /// <summary>
+        /// Returns a value that was normalised unevenly to be between <c>[-1,1]</c> to again be between the clamped range <c>[</c><paramref name="valueMin"/><c>,</c><paramref name="valueMax"/><c>]</c> where <paramref name="valueNeutral"/> represents a neutral state
+        /// </summary>
+        /// <param name="valueMin">The minumum value this function will return, will be mapped from <c>-1</c></param>
+        /// <param name="valueNeutral">The neutral value this function will return, will be mapped from <c>0</c></param>
+        /// <param name="valueMax">The maximum value this function will return, will be mapped from <c>1</c></param>
+        /// <param name="nValue">The normalised value to be projected, valid over range <c>[-1,1]</c></param>
+        /// <returns>Un-Normalised value</returns>
+        public static float UnNormaliseValueUneven(float valueMin, float valueNeutral, float valueMax, float nValue)
+        {
+            if (nValue < 0)
+            {
+                float negZero = Mathf.Clamp(nValue, -1, 0);
+                negZero += 1;
+                return Mathf.Lerp(valueMin, valueNeutral, negZero);
+            }
+            else if (nValue > 0)
+            {
+                float zeroOne = Mathf.Clamp(nValue, 0, 1);
+                return Mathf.Lerp(valueNeutral, valueMax, zeroOne);
+            }
+            else return valueNeutral;
+
+        }
+
+
     }
 }
 
